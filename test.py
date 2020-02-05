4cf3d98c610d028509a72d02a3f81a96da6e2a69
Nemolite <g25092011@mail.ru>
2020-02-05 13:38:03 +0300

    start3

--- test.py
+++ test.py
@@ -1,18 +1,17 @@
-a=2
-c=1
+count_names=['test1','test2']
+print(count_names)
 
-if (a>c):
-	print("Истино1")
-
-alocic = True
-blocic = False
-
-if (alocic==blocic):
-	print("Истино2")
-else:
-	print("Ложно2")
-
-
-	
-
-
+other_names = list(['Fred', 'Charles']) 
+print(other_names)
+#Добавление элемента в конец списка
+other_names.append('Matt') 
+print(other_names)
+#Добавление элемента в конец списка
+count_names.append('Matt3')	
+print(count_names)
+#Сливание двух спсиков
+count_names.extend(other_names)
+print(count_names)
+#'set' вписываем в список вторым
+count_names.insert(2,'set')
+print(count_names)
