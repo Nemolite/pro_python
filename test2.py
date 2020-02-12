7f82ba4de887602bd94ecf3576bd24bd66c1ef50
Nemolite <g25092011@mail.ru>
2020-02-12 14:24:46 +0300

    python_exam

--- test2.py
+++ test2.py
@@ -19,3 +19,20 @@ print("---------This is new a programm ver3-----------")
 automobil = ["volga","moskvich","jiguli","zaparojez"]
 for one_auto_index,one_auto_var in enumerate(automobil):
 	print(one_auto_index,"==",one_auto_var)
+
+
+print("---------This is example -----------")	
+
+dano = [2,7,8,11,12,3,-1,0,4,-2,5,21]
+#Найти сумму чисел больше 10
+#Найти произведение чисел меньше 10 
+#Отрицательные числа в расчет не принимать
+res_summ = 0
+res_proiz = 1
+for chislo in dano:
+	if chislo>10:
+		res_summ+=chislo
+	if (chislo<10)and(chislo>0):
+		res_proiz = res_proiz * chislo
+print(res_summ)	
+print(res_proiz)	
