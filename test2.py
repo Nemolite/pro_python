74cba8c105385b8acfb02ffe0247178f57ca0dd8
Nemolite <g25092011@mail.ru>
2020-02-12 13:40:25 +0300

    python_index

---
+++ test2.py
@@ -0,0 +1,21 @@
+for it_is_may_var in [2,3,4,5,6,7,]:
+	print(it_is_may_var)
+	print(it_is_may_var*2)
+	a = it_is_may_var + 20
+print("--------------------")	
+print(a)
+
+print("---------This is new a programm-----------")	
+
+dig = ["set1","waef","qwe","qwe"]
+for it333 in dig:
+	print(it333)
+
+print("---------This is new a programm ver 2-----------")	
+for adr in range(len(dig)):
+	print(adr,"=",dig[adr])
+
+print("---------This is new a programm ver3-----------")	
+automobil = ["volga","moskvich","jiguli","zaparojez"]
+for one_auto_index,one_auto_var in enumerate(automobil):
+	print(one_auto_index,"==",one_auto_var)
