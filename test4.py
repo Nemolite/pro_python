fd44f517c658aa9ea10fcd674188452fe525e3b7
Nemolite <g25092011@mail.ru>
2020-02-19 14:30:27 +0300

    python7

--- test4.py
+++ test4.py
@@ -1,21 +1,40 @@
-set_info = [2,4,6,-2,-3,-7,8,11,23,45]
-#Каждый из элеметов увеличиваем 1
-#и каждый раз проверяем сумму всех 
-#элементов.
-#если число стало 78947, то выходим из цикла
-result2 = 0
-while result2 < 78947:
-	#Находим сумму
-	for index in range(len(set_info)): 	
-		set_info[index] = set_info[index] + 1
-	for var1 in set_info:
-		result2 = result2 + var1
-print(set_info)
-print("-----------------")
-print(result2)
+#Новая задача
+arr_spisok = [3,9,2,4,1,-2,-5,-3,0,2,4,7]
+r = 0
+for inx in range(len(arr_spisok)):
+	r = r + arr_spisok[inx]
+print(r)
+#print(len(arr_spisok))
+r1 = 0
+print("=====================")
+for in2 in arr_spisok:
+	#print(in2)
+	r1 = r1 + in2
+print(r1)
+print("=====================")
 
+r2 = 0
+i = 0
+while i<len(arr_spisok):
+    r2 = r2 + arr_spisok[i]
+    i = i + 1
+print(r2)
 
-
+print("=====================")
+r3 = 0
+flag = True
+im = 0
+while flag:
+    r3 = r3 + arr_spisok[im]
+    #print(im)
+    im = im + 1
+    if im>len(arr_spisok)-1:
+    	flag = False
+print(r3)
+print("=====================")
+bidlo_result = 0
+bidlo_result = arr_spisok[0]+arr_spisok[1]+arr_spisok[2]+arr_spisok[3]+arr_spisok[4]+arr_spisok[5]+arr_spisok[6]+arr_spisok[7]+arr_spisok[8]+arr_spisok[9]+arr_spisok[10]+arr_spisok[11]
+print(bidlo_result)
 
 
 
