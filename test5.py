2bee9077449b5f3edf7febd876f6d978756e5f23
Nemolite <g25092011@mail.ru>
2020-02-20 17:25:57 +0300

    python8.11

--- test5.py
+++ test5.py
@@ -13,3 +13,17 @@ for n1 in names:
 	else:
 		c2[n1] += 1
 print(c2)		
+import collections
+c3 = collections.Counter(names)
+print(c3)
+
+band1_names = ['John', 'George','Paul', 'Ringo'] 
+band2_names = ['Paul'] 
+names_to_bands = {}
+for n4 in band1_names:
+	names_to_bands.setdefault(n4,[].append('Beatles'))
+for n5 in band2_names:
+	names_to_bands.setdefault(n4,[].append('Wings'))	
+print(band1_names)
+print(band2_names)
+print(names_to_bands['Paul'])
