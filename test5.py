0e7671b7581251218f23932008a3ad832994349d
Nemolite <g25092011@mail.ru>
2020-02-20 17:31:28 +0300

    python8.14

--- test5.py
+++ test5.py
@@ -1,29 +1,10 @@
-#Еще раз про словари
-names = ['Ringo','Paul','John','Ringo']
-count = {}
-for name in names:
-	count.setdefault(name, 0)
-	count[name]= count[name] + 1
-print(count)
-
-c2 = {}
-for n1 in names:
-	if n1 not in c2:
-		c2[n1] = 1
-	else:
-		c2[n1] += 1
-print(c2)		
-import collections
-c3 = collections.Counter(names)
-print(c3)
-
+import pprint
 band1_names = ['John', 'George','Paul', 'Ringo'] 
 band2_names = ['Paul'] 
 names_to_bands = {}
 for n4 in band1_names:
-	names_to_bands.setdefault(n4,[].append('Beatles'))
+	names_to_bands.setdefault(n4,[]).append('Beatles')
 for n5 in band2_names:
-	names_to_bands.setdefault(n4,[].append('Wings'))	
-print(band1_names)
-print(band2_names)
-print(names_to_bands['Paul'])
+	names_to_bands.setdefault(n5,[]).append('Wings')	
+
+pprint.pprint(names_to_bands, width=15)
