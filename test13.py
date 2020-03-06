3d3186e680af8a9faeebf396f8001ca1ca008146
Nemolite <g25092011@mail.ru>
2020-03-06 13:38:25 +0300

    python15.0.1

---
+++ test13.py
@@ -0,0 +1,22 @@
+def a1():
+	print("a1")
+def a2():
+	print("Сейчас будет вызвана функция")
+	a1()
+
+a2()
+
+def srt(a,b):
+	s=a+b
+	print(s)
+
+srt('str','1')	
+srt(2,3)
+srt('str','12')	
+
+i=0
+while i<10:
+	srt(2,2)
+	i=i+1
+    
+
---
+++ test14.py
@@ -0,0 +1,17 @@
+new_spi = ['one','two','three','one','two','three','one','two','three']
+print(new_spi[1])
+print(new_spi[-1])
+print(new_spi[-2])
+print(new_spi[-3])
+
+kor = ('one1','two1','three1')
+print(kor[1])
+print(kor[-2])
+
+
+print(new_spi[0:2])
+print(kor[0:2])
+print("===========================")
+print(new_spi[::2])
+print("===========================")
+print(new_spi[0:6:2])
