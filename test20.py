10447012f288b3f9b0debe2f05feaf936ec1dd8d
Nemolite <g25092011@mail.ru>
2020-03-17 14:12:51 +0300

    python22.1.2

---
+++ test20.py
@@ -0,0 +1,26 @@
+from ftplib import FTP
+
+ftp = FTP('localhost')
+
+print(ftp)
+
+dss = ftp.login('admin','123') 
+
+print(dss)
+
+ftp.retrlines('LIST') 
+
+filename = 'D:\\bcb\\Python\\exfile2.xlsx'
+
+f = open(filename, 'wb')
+
+print(f)
+
+send = ftp.retrbinary("STOR "+ "exfile2.xlsx", f)
+
+
+
+ftp.quit()
+
+
+
